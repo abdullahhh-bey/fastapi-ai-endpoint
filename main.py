07255859774bf694bf507220f257ea985873e2fd
Alex <kibgheeo775533@gmail.com>
2025-11-12 00:41:10 +0500

    Implemented rate limiting middleware

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI, HTTPException
 from models.models import ChatRequest, ChatRespone
 from ai.gemini import GeminiService
 from ssecrets import GEMINI_KEY
+from middleware.rateLimitingMiddleware import RateLimitingMiddleware
 
 app = FastAPI()
 
 gemini = GeminiService(api_key=GEMINI_KEY)
+
+app.add_middleware(RateLimitingMiddleware)
 
 @app.post("/ask", response_model=ChatRespone)
 async def generate_content(req: ChatRequest):
